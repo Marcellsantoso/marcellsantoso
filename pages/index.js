743ae8f06da60f96b6f83b8f0246ada90f8a148f
Marcellsantoso <marcellinus-santoso@thebodyshop.co.id>
2023-03-02 21:04:06 +0700

    porto done

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router';
+import DefaultErrorPage from 'next/error';
+import Head from 'next/head';
+import React from 'react';
+import { BuilderComponent, builder, useIsPreviewing, Builder } from '@builder.io/react';
+import '@builder.io/widgets';
+import '@builder.io/widgets/dist/lib/builder-widgets-async'
+
+
+// Initialize the Builder SDK with your organization's API Key
+// Find the API Key on: https://builder.io/account/settings
+builder.init("b8d602ecf27b40c6a613fc60a3e1012d");
+
+export async function getStaticProps({ params }) {
+    const homepage = await builder.get('homepage').toPromise();
+    return {
+        props: {
+            homepage: homepage || null,
+        },
+    };
+}
+
+export function test() {
+    console.log('hehe');
+}
+
+export default function Page({ homepage }) {
+    let asd = "none";
+    return (
+        <>
+            {/* Put your header here. */}
+            <BuilderComponent model="homepage" content={homepage}
+                data={{
+                    asd: asd
+                }
+                }
+            />
+            {/* Put your footer here. */}
+        </>
+    );
+}
